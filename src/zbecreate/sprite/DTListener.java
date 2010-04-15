a6695181f64ce7147ad1cf99965dc2b6107229c2
Dan Tracy <djt5019@gmail.com>
2010-04-15 14:14:56 -0400

    Did some work

---
+++ src/zbecreate/sprite/DTListener.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package zbecreate.sprite;
+
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+
+/**
+ *
+ * @author dan
+ */
+class DTListener implements DropTargetListener {
+
+    public void drop(DropTargetDropEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dragEnter(DropTargetDragEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dragOver(DropTargetDragEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dropActionChanged(DropTargetDragEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void dragExit(DropTargetEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+ }
